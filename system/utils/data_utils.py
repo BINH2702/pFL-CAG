96b5f79fa490593e4baf855811ac78c5d9102a62
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-03-27 20:12:14 +0900

    update

--- system/utils/data_utils.py
+++ system/utils/data_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import torch
 
-data_path = '/binh/pFL-CAG/dataset/'
+data_path = '/home/protector/binh/pFL-CAG/dataset/'
 
 def read_data(dataset, idx, niid, balance, alpha, is_train=True, num_clients=None):
     if is_train:
