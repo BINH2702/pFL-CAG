a318a4b146f897abbe6c3e21089ffd8e56a5be51
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-04-11 10:03:38 +0900

    update sh file

--- system/run_sh/emnist1.sh
+++ system/run_sh/emnist1.sh
@@ -1,7 +1,9 @@
-python main.py -log -data emnist -gr 100 -algo FedCAG -m cnn -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
-python main.py -log -data emnist -gr 200 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
-python main.py -log -data emnist -gr 400 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
+python main.py -log -data cifar100 -gr 800 -algo FedCAG -m cnn -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -nb 100
+python main.py -log -data cifar100 -gr 800 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
+python main.py -log -data cifar100 -gr 800 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 
 python main.py -log -data emnist -gr 100 -algo FedCagRod -m cnn -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 python main.py -log -data emnist -gr 200 -algo FedCagRod -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 python main.py -log -data emnist -gr 400 -algo FedCagRod -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
+
+python main.py -data cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.9 -lbs 32 -c 0.9 --noniid --balance --alpha_dirich 1 -nb 100
