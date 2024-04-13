2c4bf2202cb0a09a3535854fc964dd6b5c8a935c
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-04-13 17:39:31 +0900

    Update emnist1.sh

--- system/run_sh/emnist1.sh
+++ system/run_sh/emnist1.sh
@@ -6,7 +6,8 @@
 #python main.py -log -data emnist -gr 200 -algo FedCagRod -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 #python main.py -log -data emnist -gr 400 -algo FedCagRod -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 
-python main.py -log -data Cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.9 -lbs 32 -c 0.9 --noniid --balance --alpha_dirich 0.1 -nb 100
-python main.py -log -data Cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.9 -lbs 32 -c 0.1 --noniid --balance --alpha_dirich 0.1 -nb 100
-python main.py -log -data Cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 -c 0.9 --noniid --balance --alpha_dirich 0.1 -nb 100
-python main.py -log -data mnist -gr 800 -algo FedCagRod -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
+#python main.py -log -data Cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.9 -lbs 32 -c 0.9 --noniid --balance --alpha_dirich 0.1 -nb 100
+#python main.py -log -data Cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.9 -lbs 32 -c 0.1 --noniid --balance --alpha_dirich 0.1 -nb 100
+
+python main.py -log -data mnist -gr 800 -algo FedCagRod -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1
+python main.py -log -data Cifar100 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 -c 0.3 --noniid --balance --alpha_dirich 0.1 -nb 100
