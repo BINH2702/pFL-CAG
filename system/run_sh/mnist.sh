559c48b294bf778277ab19b74532baf8726dab1f
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-04-19 22:39:41 +0900

    Update mnist.sh

--- system/run_sh/mnist.sh
+++ system/run_sh/mnist.sh
@@ -1,11 +1,14 @@
 
 python3 main.py -log -data mnist -gr 100 -algo FedAvg -m cnn -nc 20 --noniid --balance --alpha_dirich 0.1
-python main.py -log -data mnist -gr 400 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
 
-python main.py -log -data mnist -gr 100 -algo FedCagRod -m cnn -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
-python main.py -log -data mnist -gr 200 -algo FedCagRod -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
-python main.py -log -data mnist -gr 400 -algo FedCagRod -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
 
-python main.py -data emnist -gr 100 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
 
-python3 main.py -log -data mnist -gr 100 -algo FedRod -m cnn -nc 20 --noniid --balance --alpha_dirich 0.1
+#python main.py -log -data mnist -gr 400 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
+#
+#python main.py -log -data mnist -gr 100 -algo FedCagRod -m cnn -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
+#python main.py -log -data mnist -gr 200 -algo FedCagRod -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
+#python main.py -log -data mnist -gr 400 -algo FedCagRod -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
+#
+#python main.py -data emnist -gr 100 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
+#
+#python3 main.py -log -data mnist -gr 100 -algo FedRod -m cnn -nc 20 --noniid --balance --alpha_dirich 0.1
