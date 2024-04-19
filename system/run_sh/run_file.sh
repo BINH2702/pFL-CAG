23def870cee63171d912e1c0604ce7f970736793
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-04-19 22:47:29 +0900

    Update run_file.sh

--- system/run_sh/run_file.sh
+++ system/run_sh/run_file.sh
@@ -1,8 +1,9 @@
 
-python3 main.py -log -data mnist -gr 100 -algo FedAvg -m cnn -nc 20 --noniid --balance --alpha_dirich 0.1
+python3 main.py -log -data Cifar10 -gr 100 -algo FedAvg -m resnet10 -mstr resnet10 -nc 20 --noniid --balance --alpha_dirich 0.1 -did 1
+python3 main.py -log -data Cifar10 -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
+python3 main.py -log -data Cifar10 -gr 800 -algo FedROD -m resnet10 -mstr resnet10 -nc 20 --noniid --balance --alpha_dirich 0.1 -did 1
 
-
-
+python3 main.py -log -data mnist -gr 800 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
 #python main.py -log -data mnist -gr 400 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
 #
 #python main.py -log -data mnist -gr 100 -algo FedCagRod -m cnn -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
