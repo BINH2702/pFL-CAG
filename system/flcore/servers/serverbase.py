0980304c0fba93693393a3db74edcad90382dd6a
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-04-19 23:13:01 +0900

    Update serverbase.py

--- system/flcore/servers/serverbase.py
+++ system/flcore/servers/serverbase.py
@@ -241,10 +241,10 @@ class Server(object):
 
         grad1 = params1 - prev_param
         grad2 = params2
-        print(grad1)
-        print(grad2)
         # print(f"prev:{torch.norm(prev_param)}|p1:{torch.norm(params1)}|p2:{torch.norm(params2)}")
         # print(f"g1:{torch.norm(grad1)}|g2:{torch.norm(grad2)}")
+        print(torch.dot(grad1, grad2))
+        print((torch.norm(grad1) * torch.norm(grad2)))
         cos_sim = torch.dot(grad1, grad2) / (torch.norm(grad1) * torch.norm(grad2))
         return cos_sim.item()
 
