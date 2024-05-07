15a06d72adb86c3fdb80a10bcda535f5db70f7fb
BINH2702 <92997710+BINH2702@users.noreply.github.com>
2024-05-07 15:52:36 +0900

    Update run_file.sh

--- system/run_sh/run_file.sh
+++ system/run_sh/run_file.sh
@@ -1,9 +1,10 @@
 
 #python3 main.py -log -data Cifar10 -gr 100 -algo FedAvg -m cnn -nc 20 --noniid --balance --alpha_dirich 0.1 -did 1
-python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
-python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
-
-python3 main.py -log -data mnist -gr 200 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
+#python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
+#python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
+#
+#python3 main.py -log -data mnist -gr 200 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
+#python3 main.py -log -data mnist -gr 400 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 #python3 main.py -log -data emnist -gr 200 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
 #python3 main.py -log -data Cifar10 -gr 800 -algo FedROD -m resnet10 -mstr resnet10 -nc 20 --noniid --balance --alpha_dirich 0.1 -did 1
 
@@ -17,3 +18,12 @@ python3 main.py -log -data mnist -gr 200 -algo FedCAG -m cnn -nc 40 -ls 5 -car 1
 #python main.py -data emnist -gr 100 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.3 -lbs 32 --noniid --balance --alpha_dirich 0.1
 #
 #python3 main.py -log -data mnist -gr 100 -algo FedRod -m cnn -nc 20 --noniid --balance --alpha_dirich 0.1
+
+python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1 --c_parameter 0.2
+python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1 --c_parameter 2
+
+python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1 --c_parameter 5
+python3 main.py -log -data Cifar10 -gr 100 -algo FedCAG -m resnet10 -mstr resnet10 -nc 20 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1 --c_parameter 10
+
+#python3 main.py -log -data mnist -gr 200 -algo FedCAG -m cnn -nc 40 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 0.1 -did 1
+#python3 main.py -log -data mnist -gr 400 -algo FedCAG -m cnn -nc 60 -ls 5 -car 100 -calr 25 -mmt 0.5 -ss 30 -gam 0.5 -lbs 32 --noniid --balance --alpha_dirich 1 -did 1
